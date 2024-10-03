e95d20661b72c2f9bd7ac952ec12c87a797b8117
Djalmir <djalmir_miodutzki@hotmail.com>
2024-10-03 15:07:04 -0300

    view options

--- src/components/uiElements/Map.vue
+++ src/components/uiElements/Map.vue
@@ -8,7 +8,7 @@ import { ref, onBeforeMount, onMounted, onBeforeUnmount } from 'vue'
 import L from 'leaflet'
 
 const mapContainer = ref()
-const map = ref()
+let map
 const styles = ref(null)
 
 const props = defineProps({
@@ -40,31 +40,40 @@ onBeforeMount(() => {
 })
 
 onMounted(() => {
-  map.value = L.map(mapContainer.value).setView([props.lat, props.lng], props.zoom)
+  map = L.map(mapContainer.value).setView([props.lat, props.lng], props.zoom)
 
   const osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    // attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  })
+
+  const cartoLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 20
   })
 
   const stadiaLayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
     minZoom: 0,
     maxZoom: 20,
-    // attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     ext: 'jpg'
   })
 
   const baseMaps = {
-    "OpenStreetMap": osmLayer,
-    "Stadia": stadiaLayer
+    "Simples": cartoLayer,
+    "Detalhado": osmLayer,
+    "Satélite": stadiaLayer
   }
 
-  // L.control.layers(baseMaps).addTo(map.value)
+  L.control.layers(baseMaps).addTo(map)
 
-  baseMaps["Stadia"].addTo(map.value)
 
-  if (props.marker)
-    L.marker([props.lat, props.lng]).addTo(map.value)
+  map.whenReady(() => {
+    baseMaps["Simples"].addTo(map)
+    if (props.marker)
+      L.marker([props.lat, props.lng]).addTo(map)
+  })
 })
 
 onBeforeUnmount(() => {
