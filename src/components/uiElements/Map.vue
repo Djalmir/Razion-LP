ff05d299b3cf6160c6c6000149fcab1802c5c37c
Djalmir <djalmir_miodutzki@hotmail.com>
2024-10-03 15:37:12 -0300

    voyager

--- src/components/uiElements/Map.vue
+++ src/components/uiElements/Map.vue
@@ -47,7 +47,7 @@ onMounted(() => {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   })
 
-  const cartoLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/voyager/{z}/{x}/{y}{r}.png', {
+  const cartoLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
     subdomains: 'abcd',
     maxZoom: 20
